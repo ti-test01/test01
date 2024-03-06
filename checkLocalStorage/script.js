025cbbf9621d9f9961f786e0b66796c129810022
ti-test01 <taku.test01.12345@gmail.com>
2024-03-06 11:05:19 +0900

    checkLocalStorage更新

--- checkLocalStorage/script.js
+++ checkLocalStorage/script.js
@@ -2,7 +2,12 @@ console.log("script.js")
 
 const cubismToken = "Cubism Editor Web Plugin Token"
 
-function check() {
+function writeLocalStorage() {
+    let data = "Write Local Storage. " + Date.now().toString()
+    localStorage.setItem(cubismToken, data)
+}
+
+function checkLocalStorage() {
     let data = localStorage.getItem(cubismToken)
     document.getElementById("localStorage").textContent = data
 }
